ea49841f720c53c9aad3b0fa94a2a07e4f90ea4f
shohjahon27 <xolpolatovshohjahon5@gmail.com>
2022-11-18 12:57:10 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,98 +1,224 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const drama = document.querySelector( ".promo__genre" ),
-    ads = document.querySelectorAll( ".promo__adv img " ),
-    promoBg = document.querySelector( ".promo__bg" ),
-    lists = document.querySelector( ".promo__interactive-list" ),
-    inputVal = document.querySelector( ".adding__input" ),
-    checkbox = document.querySelector( "[type='checkbox']" ),
-    submitBtn = document.querySelector( "#submit" ),
-    toastBody = document.querySelector( ".toast-body" ),
-    Spin = document.querySelector( '#spin' );
-  
-  
-  // Loading
-  const toastTrigger = document.getElementById("submit");
-  const toastLiveExample = document.getElementById( "liveToast" );
-  if ( toastTrigger ) {
-    if ( inputVal == 0 ) {
-     return inputVal.value = `bo'sh ${toastBody.value}`;
-    }
-    toastTrigger.addEventListener("click", () => {
-      const toast = new bootstrap.Toast(toastLiveExample);
+  const tabsParent = document.querySelector(".tabheader__items"),
+    tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    loader = document.querySelector(".loader");
 
-      toast.show();
+  // LOADER
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 2000);
+
+  // SLIDER
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
     });
   }
 
-  // setTimeout( () => { 
-  //   Spin.style.opacity = 0;
-  //   setTimeout( () => { 
-  //     Spin.style.display = "none";
-  //   } , 500)
-  // } , 2000);
-
-  const seriesDb = {
-    series: [
-      "OMAR",
-      "THE FINAL LEGACY",
-      "ERTUGRUL",
-      "MAGNIFICENT CENTURY",
-      "GREAT SELJUKS: GUARDIANS...",
-    ],
-  };
-
-  function makeChanges() {
-    drama.textContent = "";
-
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
   }
-  promoBg.style.backgroundImage = "url(img/1.jpg)";
 
-  const sortArr = (arr) => {
-    arr.sort();
-  };
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
 
-  submitBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    let newsLetter = inputVal.value;
-    const checkedBox = checkbox.checked;
-
-    if (newsLetter) {
-      if (newsLetter.length > 18) {
-        newsLetter = `${newsLetter.substring(0, 18)}...`;
-      }
-      if (checkedBox) {
-        alert("sevimli serial qo'shilsinmi?");
-      }
-      seriesDb.series.push(newsLetter);
-      sortArr(seriesDb.series);
-      createSeriesList(seriesDb.series, lists);
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, idx) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(idx);
+        }
+      });
     }
+  });
+  hideTabContent();
+  showTabContent();
 
-    event.target.reset(newsLetter);
+  // MODAL
+  const modaltrigger = document.querySelector("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalClosed = document.querySelector("[data-close]");
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflowY = "";
+  }
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflowY = "hidden";
+    clearInterval(timerModal);
+  }
+
+  modaltrigger.addEventListener("click", openModal);
+
+  modalClosed.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      closeModal();
+    }
   });
 
-  function createSeriesList(series, parent) {
-    parent.innerHTML = "";
-    sortArr( series );
+  document.addEventListener("keydown", (e) => {
+    if (e.code == "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
 
-    series.forEach((item, id) => {
-      parent.innerHTML += `
-      <li class="promo__interactive-item">${id + 1}. ${item}
-        <div class="delete"></div>
-    </li>
-    `;
-    });
+  // timerModal = setTimeout(openModal, 5000);
 
-    document.querySelectorAll(".delete").forEach((trash, id) => {
-      trash.addEventListener("click", () => {
-        trash.parentElement.remove();
-        seriesDb.series.splice(id, 1);
-        createSeriesList(series, parent);
-      });
-    });
+  function openModalByScrol() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener("scroll", openModalByScrol);
+    }
   }
-  sortArr(seriesDb.series);
-  makeChanges();
-  createSeriesList(seriesDb.series, lists);
+  window.addEventListener("scroll", openModalByScrol);
+
+  // TIMER PROMOTION
+  const deadline = "2022-11-15";
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const timer = Date.parse(endtime) - Date.parse(new Date());
+
+    if (timer <= 0) {
+      days = "the";
+      hours = "time";
+      minutes = "is";
+      seconds = "up";
+    } else {
+      (days = Math.floor(timer / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((timer / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((timer / 1000 / 60) % 60)),
+        (seconds = Math.floor((timer / 1000) % 60));
+    }
+
+    return { timer, days, hours, minutes, seconds };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      (days.innerHTML = getZero(t.days)),
+        (hours.innerHTML = getZero(t.hours)),
+        (minutes.innerHTML = getZero(t.minutes)),
+        (seconds.innerHTML = getZero(t.seconds));
+
+      if (t.timer <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
+
+  // CLASS CARDS
+
+  class MenuCard {
+    constructor(src, alt, title, desc, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.desc = desc;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 11000;
+      this.changeToUzs();
+    }
+
+    changeToUzs() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length == 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.desc}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Price:</div>
+              <div class="menu__item-total"><span>${this.price}</span>Uzs/month</div>
+            </div>
+          `;
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/1.png",
+    "vegy",
+    'Plan "Usual"',
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+    10,
+    ".menu .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/2.jpg",
+    "elite",
+    'Plan "“Premium”"',
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+    20,
+    ".menu .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/3.jpg",
+    "vegy",
+    'Plan "VIP"',
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.",
+    30,
+    ".menu .container",
+    "menu__item"
+  ).render();
 });
